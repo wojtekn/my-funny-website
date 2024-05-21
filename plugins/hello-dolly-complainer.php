f10f499f23c8c564819fc5bd9b8405cf7cbbea55
Wojtek Naruniec <wojtek@naruniec.me>
2024-05-21 12:18:15 +0200

    Initial commit

---
+++ plugins/hello-dolly-complainer.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * @package Hello_Dolly_Complainer
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Hello Dolly Complainer
+Plugin URI: http://wordpress.org/plugins/hello-dolly-complainer/
+Description: Let's complain a bit.
+Author: Wojtek Naruniec
+Version: 1.0.0
+Author URI: http://naruniec.me/
+*/
+
+function hello_dolly_get_complain() {
+	/** These are the lyrics to Hello Dolly */
+	$lyrics = "Really?
+Why?
+I don't think so!
+Not that pretty!
+Again an again
+Seriously?
+You've got to be kidding!
+Oh, come on!
+Not this time!
+Meh.
+Yawn...
+Is that all?
+I've heard enough.
+Not buying it.
+That doesn't make sense!
+How is that possible?
+This is getting old.
+Not impressed.
+Do we have to?
+Can we not?
+Blah blah blah";
+
+	// Here we split it into lines.
+	$lyrics = explode( "\n", $lyrics );
+
+	// And then randomly choose a line.
+	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
+}
+
+// This just echoes the chosen line, we'll position it later.
+function hello_dolly_complainer() {
+	$chosen = hello_dolly_get_complain();
+	$lang   = '';
+	if ( 'en_' !== substr( get_user_locale(), 0, 3 ) ) {
+		$lang = ' lang="en"';
+	}
+
+	printf(
+		'<p id="dollyc"><span dir="ltr"%s>%s</span></p>',
+		$lang,
+		$chosen
+	);
+}
+
+// Now we set that function up to execute when the admin_notices action is called.
+add_action( 'admin_notices', 'hello_dolly_complainer' );
+
+// We need some CSS to position the paragraph.
+function dolly_complainer_css() {
+	echo "
+	<style type='text/css'>
+	#dollyc {
+		float: right;
+		padding: 5px 10px;
+		margin: 0;
+		font-size: 9px;
+                color: red;
+		line-height: 20px;
+	}
+	.rtl #dollyc {
+		float: left;
+	}
+	.block-editor-page #dollyc {
+		display: none;
+	}
+	@media screen and (max-width: 782px) {
+		#dollyc,
+		.rtl #dollyc {
+			float: none;
+			padding-left: 0;
+			padding-right: 0;
+		}
+	}
+	</style>
+	";
+}
+
+add_action( 'admin_head', 'dolly_complainer_css' );
